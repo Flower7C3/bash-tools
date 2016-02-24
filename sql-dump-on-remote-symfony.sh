636530efb30e8ecf6b74e29d16df3a11f5feb9d2
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2016-02-24 15:11:48 +0100

    fix sql dump scripts.

---
+++ sql-dump-on-remote-symfony.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+source colors.sh
+clear
+
+_host="example-server-dev"
+_branch="dev"
+_database="example"
+
+if [ $# -ge 1 ]
+then
+  host=$1
+else
+  printf "Host [${BIYellow}${_host}${Color_Off}]: ${On_IGreen}"
+  read -e input
+  host=${input:-$_host}
+  printf "${Color_Off}"
+fi
+
+if [ $# -ge 2 ]
+then
+  branch=$2
+else
+  printf "Branch [${BIYellow}${_branch}${Color_Off}]: ${On_IGreen}"
+  read -e input
+  branch=${input:-$_branch}
+  printf "${Color_Off}"
+fi
+
+datetime=`date "+%Y%m%d-%H%M%S"`
+exportFileName="backup_${host}_${branch}_${datetime}.sql"
+remoteDataDir='${HOME}/backup/'
+localDataDir="${HOME}/Documents/lamp/"
+localScriptsDir=`pwd`"/"
+
+cd ${localDataDir}
+
+printf "Dump sql on ${BIYellow}${host}${Color_Off} from branch ${BIYellow}${branch}${Color_Off}? [n]: ${On_IGreen}"
+
+read -e input
+printf "${Color_Off}"
+run=${input:-n}
+
+if [[ "$run" == "y" ]]
+then
+
+	printf "${BBlue}Copy scripts to ${BIBlue}${host}${BBlue} host ${Blue} \n"
+	scp ${localScriptsDir}colors.sh ${host}:'${HOME}/colors.sh'
+	scp ${localScriptsDir}sql-dump-symfony.sh ${host}:'${HOME}/sql-dump-symfony.sh'
+
+	printf "${BGreen}Dump sql on ${BIGreen}${host}${BGreen} host ${Green} \n"
+	ssh ${host} '${HOME}/sql-dump-symfony.sh '${branch}' '${exportFileName}
+
+	printf "${BGreen}Copy ${BIGreen}${exportFileName}${BGreen} to local ${Green} \n"
+	scp ${host}:${remoteDataDir}${exportFileName} ${localDataDir}${exportFileName}
+
+	printf "${BRed}Cleanup ${BIRed}${host}${BRed} host ${Red} \n"
+	ssh ${host} 'rm '${remoteDataDir}${exportFileName}
+	ssh ${host} 'rm ${HOME}/colors.sh'
+	ssh ${host} 'rm ${HOME}/sql-dump-symfony.sh'
+
+  printf "${Color_Off}"
+
+fi
--- sql-dump-symfony.sh
+++ sql-dump-symfony.sh
@@ -9,11 +9,12 @@ sqlUser=`cat $configFile | sed -n "s/^    database_user:\(.*\)/\1/p" | xargs`
 sqlPass=`cat $configFile | sed -n "s/^    database_password:\(.*\)/\1/p" | xargs`
 sqlBase=`cat $configFile | sed -n "s/^    database_name:\(.*\)/\1/p" | xargs`
 
-backupFile=${sqlBase}-`date "+%Y%m%d-%H%M%S"`.sql
+backupFileDefault=${sqlBase}-`date "+%Y%m%d-%H%M%S"`.sql
+backupFile=${2:-$backupFileDefault}
 
 mkdir -p ${backupDir}
 mysqldump --host=$sqlHost --user=$sqlUser --password=$sqlPass $sqlBase > ${backupDir}${backupFile}
 
 if [[ $backupTime > 0 ]]; then
-        find ${backupDir} -mtime +${backupTime} -exec rm {} \; 
+	find ${backupDir} -mtime +${backupTime} -exec rm {} \; 
 fi
