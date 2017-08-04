a2ae319aed33d95334478cb1c1fa234986642ea2
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2017-08-04 10:57:18 +0200

    force repeat secret value and ask again if values not match

--- inc/_io.sh
+++ inc/_io.sh
@@ -18,32 +18,32 @@ function app_bye {
 ### Program info
 ###############################################################
 function program_title {
-	local title=$1
+    local title=$1
 
-	printf "${color_log_h}"
+    printf "${color_log_h}"
 
-	printf "${box_tl}${box_h}"
-	for (( c=1; c<=${#title} ; c++ ))
-	do
-	   printf "${box_h}"
-	done
-	printf "${box_h}${box_tr}\n"
+    printf "${box_tl}${box_h}"
+    for (( c=1; c<=${#title} ; c++ ))
+    do
+       printf "${box_h}"
+    done
+    printf "${box_h}${box_tr}\n"
 
-	printf "${box_v} ${title} ${box_v}\n"
+    printf "${box_v} ${title} ${box_v}\n"
 
-	printf "${box_bl}${box_h}"
-	for (( c=1; c<=${#title} ; c++ ))
-	do
-	   printf "${box_h}"
-	done
-	printf "${box_h}${box_br}\n"
-	
-	printf "${color_off}"
+    printf "${box_bl}${box_h}"
+    for (( c=1; c<=${#title} ; c++ ))
+    do
+       printf "${box_h}"
+    done
+    printf "${box_h}${box_br}\n"
+    
+    printf "${color_off}"
 }
 
 function printfln {
-	local message=$1
-	printf "${message}\n"
+    local message=$1
+    printf "${message}\n"
 }
 
 function display_info {
@@ -57,12 +57,12 @@ function display_error {
 }
 
 function program_end {
-	printf "${color_off}\n"
+    printf "${color_off}\n"
 }
 
 function program_error {
-	program_end
-	exit 1
+    program_end
+    exit 1
 }
 
 ###############################################################
@@ -71,35 +71,58 @@ function program_error {
 
 # asks user for value
 function display_prompt {
-	local password=$1
-	local variable_name=$2
-	local question=$3
-	local default_value=$4
-	local argNo=$(expr ${5:-1} + 5)
-	local args=$#
-	# get value defined in argv
-	if [ ${args} -ge ${argNo} ]; then
-	  	variable_value=${!argNo}
-	# or ask user for value
-	else
-		printf "${color_question_b}"
-		printf "(Q) ${question}"
-		if [[ -n "${default_value}" ]]; then
-			printf " [${color_question_h}${default_value}${color_question_b}]"
-		fi
-		printf ": ${color_console}"
-		if [ "$password" == "yes" ]; then
-		    read -s input
-		    printf "${color_off}"
-		    printf "\n"
-		else
-		    read -e input
-		    printf "${color_off}"
-		fi
-		# if user set nothing, then set default value
-		variable_value=${input}
-	fi
-	set_variable "$variable_name" "$default_value" "$variable_value"
+    local password=$1
+    local variable_name=$2
+    local question=$3
+    local default_value=$4
+    local argNo=$(expr ${5:-1} + 5)
+    local args=$#
+    # get value defined in argv
+    if [ ${args} -ge ${argNo} ]; then
+        variable_value=${!argNo}
+    # or ask user for value
+    else
+        if [ "$password" == "yes" ]; then
+            while true; do
+                printf "${color_question_b}"
+                printf "(Q) ${question}"
+                if [[ -n "${default_value}" ]]; then
+                    printf " [${color_question_h}${default_value}${color_question_b}]"
+                fi
+                printf ": ${color_console}"
+                read -s input1
+                printf "${color_off}"
+                printf "\n"
+                printf "${color_question_b}"
+                printf "(Q) ${question}"
+                if [[ -n "${default_value}" ]]; then
+                    printf " [${color_question_h}${default_value}${color_question_b}]"
+                fi
+                printf " - repeat: ${color_console}"
+                read -s input2
+                printf "${color_off}"
+                printf "\n"
+                if [[ "$input1" == "$input2" ]]; then
+                    input=$input1
+                    break;
+                else
+                    printf "${color_error_b}The top secret values do not match. Please retype it!\n${color_off}"
+                fi
+            done
+        else
+            printf "${color_question_b}"
+            printf "(Q) ${question}"
+            if [[ -n "${default_value}" ]]; then
+                printf " [${color_question_h}${default_value}${color_question_b}]"
+            fi
+            printf ": ${color_console}"
+            read -e input
+            printf "${color_off}"
+        fi
+        # if user set nothing, then set default value
+        variable_value=${input}
+    fi
+    set_variable "$variable_name" "$default_value" "$variable_value"
 }
 
 # asks user for variable value
@@ -114,39 +137,38 @@ function prompt_password {
 
 # asks user for variable value, but accept only allowed values
 function prompt_variable_fixed {
-	local variable_name=$1
-	local question=$2
-	local default_value=$3
-	local allowed_values=($4)
-	shift 4
-	# ask user for value from allowed list
-	while true; do
-		prompt_variable "$variable_name" "$question" "$default_value" "$@"
-		prompt_response=`eval echo '$'"${variable_name}"`
-		if test "`echo " ${allowed_values[*]} " | grep " ${prompt_response} "`"; then
-	 		break
-		else
-			printf "${color_error_b}Wrong ${color_question_b}${question}${color_error_b}. Allowed values are ${color_error_h}${allowed_values[*]}${color_error_b}!${color_off}"
-			echo ""
-		fi
-	done
+    local variable_name=$1
+    local question=$2
+    local default_value=$3
+    local allowed_values=($4)
+    shift 4
+    # ask user for value from allowed list
+    while true; do
+        prompt_variable "$variable_name" "$question" "$default_value" "$@"
+        prompt_response=`eval echo '$'"${variable_name}"`
+        if test "`echo " ${allowed_values[*]} " | grep " ${prompt_response} "`"; then
+             break
+        else
+            printf "${color_error_b}Wrong ${color_question_b}${question}${color_error_b}. Allowed values are ${color_error_h}${allowed_values[*]}${color_error_b}!\n${color_off}"
+        fi
+    done
 }
 
 # set variable value
 function set_variable {
-	local variable_name=$1
-	local default_value=$2
-	local variable_value=${3:-$default_value}
-	eval "${variable_name}"'=${variable_value}'
+    local variable_name=$1
+    local default_value=$2
+    local variable_value=${3:-$default_value}
+    eval "${variable_name}"'=${variable_value}'
 }
 
 # user must press y and enter, or program will end
 function confirm_or_exit {
-	local question=$1
-	prompt_variable_fixed run "${question}" "n" "y n"
-	printf "\n"
-	if [[ "$run" != "y" ]]
-	then
-		exit -1
-	fi
+    local question=$1
+    prompt_variable_fixed run "${question}" "n" "y n"
+    printf "\n"
+    if [[ "$run" != "y" ]]
+    then
+        exit -1
+    fi
 }
