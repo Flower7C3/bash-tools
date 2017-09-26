deb884f00b8e3fdaa54469c62ada43c12204d084
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2017-09-26 15:17:47 +0200

    text fix

--- inc/_tool_sql.sh
+++ inc/_tool_sql.sh
@@ -76,7 +76,7 @@ function mysql_remote_export_via_symfony {
 	local file_name=$3
 	file_name_check "$file_name"
 
-	printf "${color_info_b}Backup MySQL database at ${color_info_h}${host_name}${color_info_b} host in ${color_info_h}${symfony_root_dir}${color_info_b} director (via Symfony config)y${color_info}\n"
+	printf "${color_info_b}Backup MySQL database at ${color_info_h}${host_name}${color_info_b} host in ${color_info_h}${symfony_root_dir}${color_info_b} director (via Symfony config)${color_info}\n"
 	ssh ${host_name} 'symfony_config_file_path='${symfony_root_dir}'app/config/parameters.yml && sql_host=`sed -n "s/\([ ]\{1,\}\)database_host:\(.*\)/\2/p" $symfony_config_file_path | xargs`; if [[ "$sql_host" == "~" || "$sql_host" == "" || "$sql_host" == "null" ]]; then sql_host="localhost"; fi && sql_port=`sed -n "s/\([ ]\{1,\}\)database_port:\(.*\)/\2/p" $symfony_config_file_path | xargs`; if [[ "$sql_port" == "~" || "$sql_port" == "" || "$sql_port" == "null" ]]; then sql_port="3306"; fi && sql_user=`sed -n "s/\([ ]\{1,\}\)database_user:\(.*\)/\2/p" $symfony_config_file_path | xargs`; if [[ "$sql_user" == "~" || "$sql_user" == "" || "$sql_user" == "null" ]]; then sql_user="root"; fi && sql_pass=`sed -n "s/\([ ]\{1,\}\)database_password:\(.*\)/\2/p" $symfony_config_file_path | xargs` && sql_base=`sed -n "s/\([ ]\{1,\}\)database_name:\(.*\)/\2/p" $symfony_config_file_path | xargs` && mysqldump --routines --events --triggers --single-transaction --host=$sql_host --port=$sql_port --user=$sql_user --password=$sql_pass $sql_base > ${HOME}/'${file_name}''
 }
 
