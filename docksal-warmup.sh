bf7ebe3b63a2a2a3279f8d199f06453657b8948d
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2019-03-06 13:45:56 +0100

    docksal warmup: with java, fixes.

--- docksal-warmup.sh
+++ docksal-warmup.sh
@@ -2,13 +2,14 @@
 
 source $(dirname ${BASH_SOURCE})/_base.sh
 
-function copy_command {
-	local command_name=$1
-	local source_file_name=${_script_dir_path}/docksal/commands/${command_name}
-	local destination_file_name=.docksal/commands/${command_name}
-	mkdir -p $(dirname ${destination_file_name})
-	cp ${source_file_name} ${destination_file_name}
-	echo "$command_name"
+function copy_file {
+	local src_file_name=$1
+	local dst_file_name=${2:-$src_file_name}
+	local source_file_path=${_script_dir_path}/docksal/${src_file_name}
+	local destination_file_path=.docksal/${dst_file_name}
+	mkdir -p $(dirname ${destination_file_path})
+	cp ${source_file_path} ${destination_file_path}
+	printf "${color_default_i}Copied file from ${color_default_h}%s${color_default_i} to ${color_default_h}%s${color_default_i} ${color_default_i}\n" "$src_file_name" "$dst_file_name"
 }
 
 ## CONFIG
@@ -17,12 +18,14 @@ _project_name="example_$(date "+%Y%m%d_%H%M%S")"
 _project_name="test"
 _apache_version="2.4"
 _apache_versions="no 2.2 2.4"
-_php_version="7"
+_php_version="7.2"
 _php_versions="no 5.6 7.0 7.1 7.2 7.3"
 _node_version="10"
 _node_versions="no 6 8 10 11"
 _mysql_version="5.7"
 _mysql_versions="no 5.5 5.6 5.7 8.0"
+_java_version="no"
+_java_versions="no 8"
 _www_docroot="web"
 
 
@@ -39,24 +42,27 @@ if [[ "$project_name" == "." ]]; then
 fi
 prompt_variable_not domain_name "Domain name (without .docksal tld)" "$_domain_name" "." 2 "$@"
 while true; do
-    display_info "More informations on https://hub.docker.com/r/docksal/cli/"
-    prompt_variable_fixed apache_version "Apache version ($_apache_versions)" "$_apache_version" "$_apache_versions" 3 "$@"
-    prompt_variable_fixed php_version "PHP version ($_php_versions)" "$_php_version" "$_php_versions" 4 "$@"
-    prompt_variable_fixed node_version "Node version ($_node_versions)" "$_node_version" "$_node_versions" 5 "$@"
-    display_info "More informations on https://hub.docker.com/r/docksal/db/"
-    prompt_variable_fixed mysql_version "MySQL version ($_mysql_versions)" "$_mysql_version" "$_mysql_versions" 6 "$@"
+    display_info "More informations on ${color_info_h}https://hub.docker.com/r/docksal/web/${color_info_b}"
+    prompt_variable_fixed apache_version "Apache version" "$_apache_version" "$_apache_versions" 3 "$@"
+    display_info "More informations on ${color_info_h}https://hub.docker.com/r/docksal/cli/${color_info_b}"
+    prompt_variable_fixed php_version "PHP version" "$_php_version" "$_php_versions" 4 "$@"
+    prompt_variable_fixed node_version "Node version" "$_node_version" "$_node_versions" 5 "$@"
+    prompt_variable_fixed java_version "JAVA version" "$_java_version" "$_java_versions" 6 "$@"
+    display_info "More informations on ${color_info_h}https://hub.docker.com/r/docksal/db/${color_info_b}"
+    prompt_variable_fixed mysql_version "MySQL version" "$_mysql_version" "$_mysql_versions" 7 "$@"
     docksal_stack=""
     if [[ "$apache_version" != "no" && "$php_version" != "no" && "$mysql_version" != "no" ]]; then
         docksal_stack="default"
     elif [[ "$apache_version" != "no" && "$php_version" != "no" && "$mysql_version" == "no" ]]; then
         docksal_stack="default-nodb"
-    elif [[ "$apache_version" == "no" "$php_version" == "no" && "$mysql_version" == "no" && "$node_version" != "no" ]]; then
+    elif [[ "$apache_version" == "no" && "$php_version" == "no" && "$mysql_version" == "no" && "$node_version" != "no" ]]; then
         docksal_stack="node"
     fi
     if [[ "$docksal_stack" == "" ]]; then
         _apache_version="$apache_version"
         _php_version="$php_version"
         _node_version="$node_version"
+        _java_version="$java_version"
         _mysql_version="$mysql_version"
         set -- "${@:1:2}"
         display_error "Docksal stack not set. Please fix versions!"
@@ -67,23 +73,30 @@ while true; do
     fi
 done
 
-prompt_variable www_docroot "WWW dockroot" "$_www_docroot" 7 "$@"
+prompt_variable www_docroot "WWW dockroot" "$_www_docroot" 8 "$@"
 
 ## PROGRAM
-confirm_or_exit "Build configuration?"
+confirm_or_exit "Build Docksal configuration?"
 
 if [[ "$project_name" != "." ]]; then
-    printf "${color_info_b}Create ${color_info_h}%s${color_info_b} project directory ${color_info} \n" "$project_name"
+    printf "${color_info_b}Create ${color_info_h}%s${color_info_b} project directory ${color_default_i} \n" "$project_name"
     mkdir -p ${project_name}
     cd ${project_name}
     printf "${color_off}"
 fi
 
+if [[ -d .docksal ]]; then
+    display_error "Docksal config already exists!"
+    confirm_or_exit "Override Docksal configuration?"
+fi
+
 printf "${color_info_b}Create basic configuration \n"
 fin config generate --docroot=${www_docroot} --stack=${docksal_stack}
 fin config set VIRTUAL_HOST="${domain_name}"
-fin config set WEB_IMAGE="docksal/web:2.1-apache${apache_version}"
-fin config set CLI_IMAGE="docksal/cli:2.5-php${php_version}"
+docksal_web_image="docksal/web:2.1-apache${apache_version}"
+fin config set WEB_IMAGE="$docksal_web_image"
+docksal_cli_image="docksal/cli:2.5-php${php_version}"
+fin config set CLI_IMAGE="$docksal_cli_image"
 if [[ "$mysql_version" != "no" ]]; then
 	fin config set DB_IMAGE="docksal/db:1.1-mysql-${mysql_version}"
 fi
@@ -91,18 +104,28 @@ if [[ "$node_version" != "no" ]]; then
     # see https://github.com/creationix/nvm#nvmrc
 	echo ${node_version} > .nvmrc
 fi
+echo "services:" >> .docksal/docksal.yml
 printf "${color_off}"
 
-printf "${color_info_b}Add custom commands ${color_info} \n"
+printf "${color_info_b}Add custom commands ${color_default_i} \n"
 if [[ "$node_version" != "no" ]]; then
-	copy_command "gulp"
-	copy_command "npm"
+	copy_file "commands/gulp"
+	copy_file "commands/npm"
 fi
-copy_command "init"
-copy_command "init-site"
+copy_file "commands/init"
+copy_file "commands/init-site"
 printf "${color_off}"
 
-printf "${color_info_b}Start project (execute ${color_info_h}%s${color_info_b} command) ${color_info} \n" "fin start"
+if [[ "$java_version" != "no" ]]; then
+    printf "${color_info_b}Add ${color_info_bi}JAVA${color_info_b} to ${color_info_bi}cli${color_info_b} container ${color_default_i} \n"
+    mkdir -p .docksal/services/cli/
+    copy_file "services/cli/Dockerfile-with-java" "services/cli/Dockerfile"
+    sed -i '' -e "s/FROM \(.*\)/FROM "$(echo "$docksal_cli_image" | sed 's/\//\\\//g' )"/g" .docksal/services/cli/Dockerfile
+    cat ${_script_dir_path}/docksal/docksal-cli-java.yml.part >> .docksal/docksal.yml
+    printf "${color_off}"
+fi
+
+printf "${color_info_b}Start project (execute ${color_info_h}%s${color_info_b} command) ${color_default_i} \n" "fin start"
 fin init
 printf "${color_off}"
 
